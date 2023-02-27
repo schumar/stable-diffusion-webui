7c8db6c9f84f7efb446ef8e94ef0ed2c0764c521
Martin Schuster <schuster@isja.org>
2023-02-27 16:51:45 +0100

    retrieve_civitai: Remove useless png-d/l functionality

--- retrieve_civitai.pl
+++ retrieve_civitai.pl
@@ -1,24 +1,16 @@
 #!/usr/bin/perl
 #
+# Loads the given (civitai)-URL, extracts the JSON from the page and puts
+# it into a file in models/meta/
 
 use v5.32;
 use warnings;
 use HTTP::Tiny;
-use JSON;
-use Data::Dumper;
 
 my $authcookie = 'SET_ME';
 my $imgcache = 'https://imagecache.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/';
 my $basedir = '/local/stable-diffusion-webui/';
-my @sfwdir = ('_SFW', '_NSFW');
-
-my %dir_of_type = (
-    'LORA' => "$basedir/models/Lora",
-    'TextualInversion' => "$basedir/embeddings",
-    'Checkpoint' => "$basedir/models/Stable-diffusion",
-    'Hypernetwork' => "$basedir/models/hypernetworks",
-    'Controlnet' => "$basedir/models/ControlNet",
-);
+my $metadir = "$basedir/models/meta";
 
 my $cache_html = 0;
 my $cache_json = 1;
@@ -26,8 +18,6 @@ my $cache_json = 1;
 my $ua = HTTP::Tiny->new(
     default_headers => { Cookie => "__Host-next-auth.csrf-token=$authcookie" }
 );
-
-my $json_codec = JSON->new->allow_nonref;
 
 while (my $url = shift @ARGV) {
     die unless $url =~ m!(\d+)/([^/]+)$!;
@@ -68,67 +58,6 @@ while (my $url = shift @ARGV) {
         close $fh;
     }
 
-    my $meta = $json_codec->decode($json);
-
-    my $data = $$meta{props}{pageProps}{trpcState}{json}{queries}[0]{state}{data};
-    my $name = $$data{name};
-    my $type = $$data{type};
-    my $nsfw = exists $$data{nsfw} ? $$data{nsfw} : 0;
-
-    printf STDERR "Name: %s (%s)%s\n", $name, $type, $nsfw ? ' !NSFW!' : '';
-
-
-    ### Find location of model file (so we can add a Preview)
-
-    # but not for Poses
-    next if $type eq 'Poses';
-
-    unless ( exists $dir_of_type{$type} and -d "$dir_of_type{$type}" ) {
-        printf STDERR "    Unknown type %s, skipping.\n", $type;
-        next;
-    }
-
-    (my $camel_name = $name) =~ s/ (.)/uc($1)/eg;   # camel case
-    $camel_name =~ s/^(.)/lc($1)/e;                 # first char lower-case
-    $camel_name =~ s/[^a-z0-9]//gi;                 # remove forbidden chars
-
-    my $version = $$meta{props}{pageProps}{trpcState}{json}{queries}[0]{state}{data}{modelVersions}[0];
-    (my $ver_name = $$version{name}) =~ s/[^a-z0-9]//gi;
-    $ver_name = lc $ver_name;
-
-    my $basefn = "${camel_name}_${ver_name}";
-    $basefn = lc $$version{trainedWords}[0] if $type eq 'TextualInversion' and exists $$version{trainedWords} and defined $$version{trainedWords}[0];
-    $basefn =~ s/\..*//;
-    my $basepath = "$dir_of_type{$type}/$sfwdir[$nsfw]/$basefn";
-
-    # Search for model
-    for my $sfwdir (@sfwdir) {
-        opendir my $dh, "$dir_of_type{$type}/$sfwdir" or next;
-        while (readdir $dh) {
-            next if /^\./;
-            if ($_ =~ /^(\Q$basefn\E)\.(.+)$/i) {
-                $basepath = "$dir_of_type{$type}/$sfwdir/$1";
-                last;
-            }
-        }
-        closedir $dh;
-    }
-
-    printf STDERR "    path: %s\n", $basepath;
-
-    my $img_fn = "$basepath.png";
-    $img_fn = "$basepath.preview.png";
-    my $img = $$version{images}[0];
-
-    if ( ! -e $img_fn ) {
-        my $response = $ua->get("$imgcache/$$img{url}/width=$$img{width}");
-        die "Failed: $response->{status} $response->{reason}." unless $response->{success};
-        my $content = $response->{content};
-        open my $fh, '>', $img_fn or die "$!.";
-        print $fh $content;
-        close $fh;
-        printf STDERR "    downloaded %s\n", $img_fn;
-    }
-
+    printf STDERR "Retrieved %s (%s)\n", $name, $type;
 }
 
