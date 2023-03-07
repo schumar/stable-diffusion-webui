5232cab35c697e8b8e3680e3bebcf8e83751b258
Martin Schuster <schuster@isja.org>
2023-03-07 16:39:20 +0100

    ENH: Show displayname in extra_networks/hypernetworks

--- modules/ui_extra_networks_hypernets.py
+++ modules/ui_extra_networks_hypernets.py
@@ -2,6 +2,7 @@ import json
 import os
 
 from modules import shared, ui_extra_networks
+from modules.hypernetworks import hypernetwork
 
 
 class ExtraNetworksPageHypernetworks(ui_extra_networks.ExtraNetworksPage):
@@ -12,8 +13,9 @@ class ExtraNetworksPageHypernetworks(ui_extra_networks.ExtraNetworksPage):
         shared.reload_hypernetworks()
 
     def list_items(self):
-        for name, path in shared.hypernetworks.items():
-            path, ext = os.path.splitext(path)
+        hypernetwork: hypernetwork.HypernetworkInfo
+        for title, hypernetwork in sorted(shared.hypernetworks_full.items(), key=itemgetter(0)):
+            path, ext = os.path.splitext(hypernetwork.filename)
             previews = [path + ".png", path + ".preview.png"]
 
             preview = None
@@ -22,11 +24,19 @@ class ExtraNetworksPageHypernetworks(ui_extra_networks.ExtraNetworksPage):
                     preview = self.link_preview(file)
                     break
 
+            name = hypernetwork.name_for_extra
+            displayname = name
+            if hypernetwork.meta:
+                if hypernetwork.meta.get('displayname'):
+                    displayname = hypernetwork.meta['displayname']
+                elif hypernetwork.meta.get('title'):
+                    displayname = hypernetwork.meta['title']
+
             yield {
-                "name": name,
-                "filename": path,
+                "name": displayname,
+                "filename": hypernetwork.filename,
                 "preview": preview,
-                "search_term": self.search_terms_from_path(path),
+                "search_term": self.search_terms_from_path(hypernetwork.filename),
                 "prompt": json.dumps(f"<hypernet:{name}:") + " + opts.extra_networks_default_multiplier + " + json.dumps(">"),
                 "local_preview": path + ".png",
             }
