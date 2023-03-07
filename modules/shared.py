f7677389276c65b3b14ffc27dd268231fa7e7e42
Martin Schuster <schuster@isja.org>
2023-03-07 16:36:08 +0100

    ENH: read metadata for hypernetworks
    This is much bigger than the other changes, because the existing list
    of hypernetworks is just a dictionary of name=>filename, so we can't
    touch that w/o breaking existing extensions.
    Created a new, additional list "hypernetworks_full".

--- modules/shared.py
+++ modules/shared.py
@@ -156,6 +156,7 @@ config_filename = cmd_opts.ui_settings_file
 
 os.makedirs(cmd_opts.hypernetwork_dir, exist_ok=True)
 hypernetworks = {}
+hypernetworks_full = {}
 loaded_hypernetworks = []
 
 
@@ -163,6 +164,7 @@ def reload_hypernetworks():
     from modules.hypernetworks import hypernetwork
     global hypernetworks
 
+    # list_hypernetworks() will also refresh shared.hypernetworks_full
     hypernetworks = hypernetwork.list_hypernetworks(cmd_opts.hypernetwork_dir)
 
 
